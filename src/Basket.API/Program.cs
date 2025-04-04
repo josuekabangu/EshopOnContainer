a647f93d7cbb46bf7a242b1b884f1c5f11ae07b4
ajkabs <ajkabs2@gmail.com>
2025-04-04 05:31:43 +0000

    first commit

---
+++ src/Basket.API/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure Kestrel to support both HTTP/1.x and HTTP/2
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 8080, listenOptions =>
+    {
+        // Support both HTTP/1.x and HTTP/2
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+    });
+});
+
+builder.AddBasicServiceDefaults();
+builder.AddApplicationServices();
+
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+app.MapGrpcService<BasketService>();
+
+app.Run();
