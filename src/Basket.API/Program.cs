6b758fdcfe656327bcdfbe65c80527ea25587163
josuekabangu <62915458+josuekabangu@users.noreply.github.com>
2025-04-10 11:26:37 +0000

    [Docker] - docker-compose up and service health

--- src/Basket.API/Program.cs
+++ src/Basket.API/Program.cs
@@ -3,12 +3,11 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure Kestrel to support both HTTP/1.x and HTTP/2
+// Configure Kestrel sans spécifier un port fixe
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.Listen(IPAddress.Any, 8080, listenOptions =>
+    options.ConfigureEndpointDefaults(listenOptions =>
     {
-        // Support both HTTP/1.x and HTTP/2
         listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
     });
 });
