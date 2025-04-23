89ee442cd9968b2c4d15343941e3fef3035a68a3
josuekabangu <ajkabs2@gmail.com>
2025-04-23 16:58:50 +0200

    laste version

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
+
 echo "Waiting for PostgreSQL to start..."
 until pg_isready -U postgres -d catalogdb; do
   sleep 1
 done
-echo "Creating vector extension..."
+
+echo "Creating vector extension in catalogdb..."
 psql -U postgres -d catalogdb -c "CREATE EXTENSION IF NOT EXISTS vector;"
+
+echo "Creating identitydb if not exists..."
+psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'identitydb'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE identitydb;"
+
+echo "Database identitydb created (if it wasn't already)."
